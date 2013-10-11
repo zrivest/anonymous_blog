cf78b162a0eff0a93f05b25b5d6b0c70fee1d146
zrivest <zrivest@gmail.com>
2013-10-11 09:32:37 -0500

    another ajax

--- app/controllers/index.rb
+++ app/controllers/index.rb
@@ -9,4 +9,14 @@ get "/delete/:id" do
   Post.find(params[:id]).destroy
   redirect '/'
 end
+
+get '/all/posts' do
+  # binding.pry
+  @all_posts = Post.all
+  if request.xhr?
+    erb :_all_posts, layout: false
+  else
+    erb :all_posts
+  end
+end
 #=============POST=============#
--- public/js/application.js
+++ public/js/application.js
@@ -9,3 +9,15 @@ $(document).ready(function() {
     });
   });
 });
+
+$(document).ready(function() {
+  $("#show_all_posts_link").click(function(event){
+    event.preventDefault();
+    
+    var url = $(this).attr("href");
+    $.get(url, function(response){
+      console.log(response);
+      $("#show_all_posts_link").after(response)
+    });
+  });
+});
